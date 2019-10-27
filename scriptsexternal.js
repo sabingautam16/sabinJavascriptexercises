ee565afb515ddad3bd18c7b942b1885736d66f8c
Sabin <sabingautam16@yahoo.com>
2019-10-27 14:21:48 -0700

    made soem changes

---
+++ scriptsexternal.js
@@ -0,0 +1,17 @@
+function mango() {
+  document.getElementById("mellow").innerHTML = "Trying something else";
+}
+function mango1() {
+  document.write(5 + 6);
+}
+function al() {
+  window.alert(5 + 3);
+}
+var x, y, z;
+x = 2;
+y = 3;
+z = x * y + y;
+console.log(z);
+function myFunc() {
+  document.getElementById("mellow1").innerHTML = z;
+}
